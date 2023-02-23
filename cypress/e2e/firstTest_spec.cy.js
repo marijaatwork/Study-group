131283fbc9918b66af179994e58f5e54fc4aaf0e
Marija Matavulj <marijamatavulj@BEG-Marija-Matavulj.local>
2023-02-23 11:33:37 +0100

    test modified based on Tamara's comment

--- cypress/e2e/firstTest_spec.cy.js
+++ cypress/e2e/firstTest_spec.cy.js
@@ -2,7 +2,7 @@
 describe ('My First Test',() => {
     it ('Visits Work & Co website and checks the headeline.', () => {
         cy.visit('https://work.co')
-        cy.get("[data-test-id='header-title-text']").contains('We design and ship digital products that transform companies.')
+        cy.get("[data-test-id='header-title-text']").should('have.text', 'We design and ship digital products that transform companies.')
     })
     })
 
