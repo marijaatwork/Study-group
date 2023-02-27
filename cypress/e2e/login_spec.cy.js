89e7f1f839b8f921f025ea4a1c5f5aef8faf0cc1
Marija Matavulj <marijamatavulj@BEG-Marija-Matavulj.local>
2023-02-27 17:32:27 +0100

    challenge 4 update

--- cypress/e2e/login_spec.cy.js
+++ cypress/e2e/login_spec.cy.js
@@ -16,6 +16,6 @@ describe('Challenge 4', () => {
         cy.get("[data-test='username']").type('standard_user')
         cy.get("[data-test='password']").type('secret_sauce')
         cy.get("[data-test='login-button']").click()
-        cy.url().should('include', '/inventory.html')
+        cy.url().should('include', '/inventory.html') 
     })
     })
