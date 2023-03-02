3622ad2460a22ec08427441481d45abbed8dfb4c
Marija Matavulj <marijamatavulj@BEG-Marija-Matavulj.local>
2023-03-02 12:23:26 +0100

    cart.cy.js file updated based on the comments

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,10 +1,8 @@
 describe('challenge 5', () => {
     beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get("[data-test='username']").type('standard_user')
-        cy.get("[data-test='password']").type('secret_sauce')
-        cy.get("[data-test='login-button']").click()
-    })
+            cy.login('standard_user', 'secret_sauce')
+          })
+
         it('tc1 error with no data', () => {
           cy.get("[id='shopping_cart_container']").click().end().get("[data-test='checkout']").click()
           cy.get("[data-test='continue']").click()
